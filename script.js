3bb6f2debbd20da1589447387bcaae12cb8e524c
Michael Catrone <47573331+MikeCatrone@users.noreply.github.com>
2021-03-19 14:06:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 
-
+let open = true;
 
 $(".testButton").click(() => {
     $(".portContainer").animate({ width: "30%", height: "120px" });
     $("h1").hide();
     $("p").hide();
     $("video").hide();
+    open = !open;
+    
+    if(open === false) {
+        $(".portContainer").animate({ maxWidth: "30%", height: "62%" });
+        $("h1").show();
+        $("p").show();
+        $("video").show();
+    }
     
 })

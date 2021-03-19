b53766f757a8d142730de493d7832cf91e19ccda
Michael Catrone <47573331+MikeCatrone@users.noreply.github.com>
2021-03-19 14:34:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,7 @@ $(".testButton").click(() => {
     
     if(open === false) {
         $(".portContainer").animate({ width: "60%", height: "62%" });
-        $("h1").show();
-        $("p").show();
-        $("video").show();
+       
         open = !open;
     } else {
         $(".portContainer").animate({ width: "30%", height: "120px" });

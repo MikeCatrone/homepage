605eca1dfa889f94ac42f7a76a31b2f5673a6fb2
Michael Catrone <47573331+MikeCatrone@users.noreply.github.com>
2021-03-19 12:37:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 $(".testButton").click(() => {
     $(".portContainer").animate({ width: "200px", height: "120px" });
-    $("h1").remove();
+    $(".headingText").remove();
     $("p").remove();
     $("video").remove();
     

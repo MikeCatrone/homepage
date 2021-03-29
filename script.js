019e23a4fff611c198081e0d7754de812fed8a98
Michael Catrone <47573331+MikeCatrone@users.noreply.github.com>
2021-03-29 12:01:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let open = false;
         $(".portContainer").css("height", "220px" );
         $("h1").text('React Native Coffee Data');
         $("p").hide();
-        $("img").hide();
+        $(".coffeeImage").hide();
 
 
 $(".testButton").click(() => {
@@ -14,14 +14,14 @@ $(".testButton").click(() => {
         $(".portContainer").animate({ width: "1200px", height: "100%" });
         $("h1").show();
         $("p").show();
-        $("img").show();
+        $(".coffeeImage").show();
        
         open = !open;
     } else {
         $(".portContainer").animate({ width: "400px", height: "200px" });
         $("h1").text('My Coffee App');
         $("p").hide();
-        $("img").hide();
+        $(".coffeeImage").hide();
         open = !open;
     }
     

ec83f747c5b1aa42e372c5343e19de8eaf93c60d
Michael Catrone <47573331+MikeCatrone@users.noreply.github.com>
2023-10-10 10:49:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+
 /////////////////////////////////////////////////////
 console.log(" Conan O'Brien: 'Greetings, Nerds!' ");
 /////////////////////////////////////////////////////
@@ -12,7 +13,10 @@ let state = false;
 let buttonTarget = document.querySelector('.portButton');
 let thePort =  document.querySelector('.portWindow');
 let theImg = document.querySelector('.pokePic');
+let theImg2 = document.querySelector('.shoppingPic');
 let theSnippet = document.querySelector('.snippet');
+let jsLogo = document.querySelector('.jsLogo');
+let reactLogo = document.querySelector('.reactLogo');
 
 
 buttonTarget.addEventListener('click',() => {
@@ -21,6 +25,9 @@ buttonTarget.addEventListener('click',() => {
 
     state ? thePort.setAttribute('class', 'portWindow2') : thePort.setAttribute('class', 'portWindow');
     state ? theImg.setAttribute('class', 'pokePic2') : theImg.setAttribute('class', 'pokePic');
-    state ? theSnippet.setAttribute('class','snippet2') : theSnippet.setAttribute('class', 'snippet')
+    state ? theSnippet.setAttribute('class','snippet2') : theSnippet.setAttribute('class', 'snippet');
+    state ? theImg2.setAttribute('class', 'shoppingPic2') : theImg2.setAttribute('class', 'shoppingPic');
+    state ? jsLogo.setAttribute('class', 'jsLogo2') : jsLogo.setAttribute('class', 'jsLogo');
+    state ? reactLogo.setAttribute('class', 'reactLogo2') : reactLogo.setAttribute('class', 'reactLogo');
 
 })
